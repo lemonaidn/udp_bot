a1d9da61eb78e3f0b450fecb3fdc68b287a26a05
Aidan Hughes <72153806+lemonaidn@users.noreply.github.com>
2024-04-03 20:56:11 +0000

    adding timestamp to output filenames and .fec filenames to output data

--- extract_desired_data.py
+++ extract_desired_data.py
@@ -1,12 +1,18 @@
 import os
 import csv
 import fecfile
+from datetime import datetime
 
 # Define the directory where .fec files are stored
 directory_path = 'fec_files'
 
-# Define the CSV file path for storing the results
-csv_file_path = 'last_twenty_ind_expenditures_df.csv'
+# Generate a timestamp
+timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
+
+# Define the CSV file path for storing the results, appending the timestamp
+csv_file_base = 'last_twenty_ind_expenditures_df'
+csv_file_extension = '.csv'
+csv_file_path = f'{csv_file_base}_{timestamp}{csv_file_extension}'
 
 # Prepare to collect data from all .fec files
 all_data = []
@@ -32,13 +38,14 @@ for filename in os.listdir(directory_path):
                 ytd_amount = item.get('calendar_y_t_d_per_election_office')
                 election = 'Primary' if item.get('election_code') == 'P2024' else 'General'
                 
-                # Add the extracted information to the all_data list
-                all_data.append([amount, candidate_name, support_oppose, district, state, purpose, ytd_amount, election])
+                # Add the extracted information to the all_data list along with the filename
+                all_data.append([filename, amount, candidate_name, support_oppose, district, state, purpose, ytd_amount, election])
 
 # Write the collected data to a CSV file
 with open(csv_file_path, mode='w', newline='', encoding='utf-8') as csv_file:
     writer = csv.writer(csv_file)
-    writer.writerow(['Amount', 'Candidate Name', 'Support/Oppose', 'District', 'State', 'Purpose', 'YTD Amount', 'Election'])
+    # Add 'Filename' to the header row
+    writer.writerow(['Filename', 'Amount', 'Candidate Name', 'Support/Oppose', 'District', 'State', 'Purpose', 'YTD Amount', 'Election'])
     writer.writerows(all_data)
 
 print(f"Data extracted to CSV file: {csv_file_path}")
