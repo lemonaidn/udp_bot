aa3bce6c81a89908bba31db77a2d96ec5607a413
Aidan Hughes <72153806+lemonaidn@users.noreply.github.com>
2024-04-21 19:24:20 +0000

    testing twitter auth

---
+++ tweepy-bots/twitterbot_scratchpad.py
@@ -0,0 +1,14 @@
+import tweepy
+import os
+
+auth = tweepy.OAuthHandler(os.environ.get('TWITTER_CONSUMER_API_KEY'), os.environ.get('TWITTER_CONSUMER_API_KEY'))
+
+auth.set_access_token(tweepy.OAuthHandler(os.environ.get('TWITTER_AUTH_ACCESS_TOKEN'), os.environ.get('TWITTER_AUTH_ACCESS_TOKEN_SECRET')))
+
+api = tweepy.API(auth)
+
+try:
+    api.verify_credentials()
+    print("Authentication OK")
+except:
+    print("Error during authentication")
